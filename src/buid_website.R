7542af9fac8322c1a743d65e82aeb538b20ce496
Keven Bluteau <Keven.bluteau@unine.ch>
2021-01-12 17:09:53 -0500

    update 4

--- src/buid_website.R
+++ src/buid_website.R
@@ -1,12 +1,11 @@
 blogdown::install_hugo("0.50", force = TRUE)
-blogdown::serve_site()
+blogdown::build_site()
 file = list.files("../",recursive = FALSE)
 file = file[!stringr::str_detect(file,"src")]
 file = file[!stringr::str_detect(file,"WebSite\\.Rproj")]
 unlink(x = paste0("../",file), recursive = TRUE)
-install.
+
 file = list.files("public/")
 file.copy(paste0("./public/",file),to = "../",recursive = TRUE)
 file.create('../.nojekyll')
 unlink("public",recursive = TRUE)
-blogdown::install_theme("gcushen/hugo-academic")
